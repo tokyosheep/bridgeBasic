bc89a641918ec998d13cf1e711cf951da02333c8
Ink_004 <ink_004@Inkjet-Yamane-no-Mac-Pro-2.local>
2019-08-16 17:41:18 +0900

    updata

--- js/main.js
+++ js/main.js
@@ -5,12 +5,32 @@ window.onload = () =>{
     const btn_test = document.getElementById("btn_test");
     const dropArea = document.getElementById("dropArea");
     const fileList = document.getElementById("fileList");
+    const redLabel = document.getElementById("redLabel");
+    const reset = document.getElementById("reset");
     const path = require("path");
+    const filePath = csInterface.getSystemPath(SystemPath.EXTENSION) +`/js/`;
+    const extensionRoot = csInterface.getSystemPath(SystemPath.EXTENSION) +`/jsx/`;
+    csInterface.evalScript(`$.evalFile("${extensionRoot}json2.js")`);//json2読み込み
+    
     console.log(__dirname);
     
+    reset.addEventListener("click",resetDrop);
     
-    btn_test.addEventListener("click",()=>{
-        csInterface.evalScript(`alert("Oh my gods")`);
+    redLabel.addEventListener("click",()=>{
+        const labelNumber = Array.from(document.getElementsByClassName("labelColor")).findIndex(v => v.checked);
+        console.log(labelNumber);
+        const liList = Array.from(dropArea.getElementsByTagName("li"));
+        const pathList = liList.map(v =>  v.dataset.path);
+        const obj = {
+            pathList:pathList,
+            number:labelNumber
+        }
+        csInterface.evalScript(`addLabel(${JSON.stringify(obj)})`,result=>{
+            console.log(result);
+            if(result){
+                resetDrop();
+            }
+        });
     });
     
     prevent_drag_event();
@@ -34,9 +54,11 @@ window.onload = () =>{
             e.preventDefault();
             const files = Array.from(e.dataTransfer.files);
             console.log(files);
+            /*
             while(this.area.firstChild){
                 this.area.removeChild(this.area.firstChild);
             }
+            */
             files.forEach(v=>{
                 const li = document.createElement("li");
                 li.textContent = v.name;
@@ -55,6 +77,12 @@ window.onload = () =>{
         function prevent_dragnaddrop(e){
             e.stopPropagation();
             e.preventDefault();
+        }
+    }
+    
+    function resetDrop(){
+        while(dropArea.firstChild){
+            dropArea.removeChild(dropArea.firstChild);
         }
     }
     
--- jsx/hostscript.jsx
+++ jsx/hostscript.jsx
@@ -1,7 +1,44 @@
-/*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
-/*global $, Folder*/
+/*
+var array = ["/Users/ink_004/Desktop/0816/633937_パールマネキン_コロンビア_ヒマラヤちはら台エンド什器_マグネットシート_INK/出力/oki_1220_050_マグネットシート_902x199.pdf"];
+addLabel(array);
+*/
+function addLabel(obj){
+    var prefs = app.preferences;
+    //synchronousMode = true
+    var doc = app.document;
+    var allFileList = doc.thumbnail.children;
+    var MyLabels = getLabels();
 
+    for(var j=0;j<allFileList.length;j++){
+        if(checkPath(allFileList[j],obj.pathList)){
+            allFileList[j].label = MyLabels[obj.number];
+        }
+    }
+    
+    function allAddRed(){
+        for(var i=0;i<allFileList.length;i++){
+            $.writeln("path:" + allFileList[i].path);
+            allFileList[i].label = MyLabels[0];
+        }
+    }
 
-function sayHello(){
-    alert("hello from ExtendScript");
+    function checkPath(file,array){
+        for(var n =0;n<array.length;n++){
+            if(file.path == array[n]){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function getLabels(){
+        var labelNames = ["Label1","Label2","Label3","Label4","Label5"];
+        var yourLabels = [];
+        for(var n=0;n<labelNames.length;n++){
+            yourLabels[n] = prefs[labelNames[n]];
+            $.writeln(prefs[labelNames[n]]);
+        }
+        return yourLabels;
+    }
+    return true;
 }
